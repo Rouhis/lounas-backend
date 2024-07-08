c5cbefafe5eb5103c2aca66865db63fe78693eae
rouhis <eeturouhiainen1@gmail.com>
2024-07-08 10:33:00 +0300

    re push main

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def clean_menu(menu):
 @app.route('/menus', methods=['GET'])
 def get_menus():
     menus = []
-    arden.get_menu()
+    #arden.get_menu()
     # Get menus from all restaurant scrapers and clean them
-    #menus.append({'restaurant': 'Ninan Keittiö', 'menu': clean_menu(ninanKeittio.get_menu()['menu'])})
-    #menus.append({'restaurant': 'Dylan Böle', 'menu': clean_menu(dylan.get_menu()['menu'])})
-    #menus.append({'restaurant': 'Factory Pasila', 'menu': clean_menu(factoryPasila.get_menu()['menu'])})
-    #menus.append({'restaurant': 'Iso Paja', 'menu': clean_menu(isopaja.get_menu()['menu'])})
-    #menus.append({'restaurant': 'Delhi Rasoi', 'menu': clean_menu(delhirasoi.get_menu()['menu'])})
+    menus.append({'restaurant': 'Ninan Keittiö', 'menu': clean_menu(ninanKeittio.get_menu()['menu'])})
+    menus.append({'restaurant': 'Dylan Böle', 'menu': clean_menu(dylan.get_menu()['menu'])})
+    menus.append({'restaurant': 'Factory Pasila', 'menu': clean_menu(factoryPasila.get_menu()['menu'])})
+    menus.append({'restaurant': 'Iso Paja', 'menu': clean_menu(isopaja.get_menu()['menu'])})
+    menus.append({'restaurant': 'Delhi Rasoi', 'menu': clean_menu(delhirasoi.get_menu()['menu'])})
     # Add calls to other restaurant scraper functions as needed
     
     return jsonify({'menus': menus})
